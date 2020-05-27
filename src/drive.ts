33857c1cbf07a54c2435626a10a0c4e3b8f3551b
Stewart <stewartcallummcgown@gmail.com>
2020-05-28 00:44:48 +0100

    feat(): readme and base64 key

--- src/drive.ts
+++ src/drive.ts
@@ -1,16 +1,15 @@
 import { google } from 'googleapis';
-import * as privatekey from '../service-account.json';
 import { Stream, Readable } from 'stream';
 
 export const getAuth = () => {
+const privatekey = JSON.parse(Buffer.from(process.env.GOOGLE_PRIVATE_KEY, 'base64').toString('ascii'))
+
   const jwtClient = new google.auth.JWT(
     privatekey.client_email,
     null,
     privatekey.private_key,
     [
-      'https://www.googleapis.com/auth/spreadsheets',
       'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/calendar',
     ],
   );
 
@@ -26,6 +25,29 @@ export const getAuth = () => {
   return jwtClient;
 };
 
+function humanFileSize(bytes, si=false, dp=1) {
+    const thresh = si ? 1000 : 1024;
+  
+    if (Math.abs(bytes) < thresh) {
+      return bytes + ' B';
+    }
+  
+    const units = si 
+      ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+      : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+    let u = -1;
+    const r = 10**dp;
+  
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+  
+  
+    return bytes.toFixed(dp) + ' ' + units[u];
+  }
+  
+
 export const resolvePath = async (path: string): Promise<Stream> => {
   const files = google.drive('v3').files;
   const auth = await getAuth();
@@ -40,7 +62,7 @@ export const resolvePath = async (path: string): Promise<Stream> => {
     const result = await files.list({
       q: `'${file.id}' in parents and name = '${parts[i]}'`,
       pageSize: 1000,
-      fields: 'files(id,mimeType,name, size)'
+      fields: 'files(id,mimeType,name,size)'
     });
 
     const match = result.data.files[0];
@@ -65,10 +87,10 @@ export const resolvePath = async (path: string): Promise<Stream> => {
     const result = await files.list({
         q: `'${file.id}' in parents`,
         pageSize: 1000,
-        fields: 'id,mimeType,name,size'
+        fields: 'files(id,mimeType,name,size,modifiedTime)'
       });
 
-    return Readable.from('<div>' + result.data.files.map(f => `<h1>${f.name}</h1>`).join(' ') + '</div>')
+    return Readable.from(`<h1>/${path}</h1><table><thead><th>Name</th><th>Modifier</th><th>Size</th></thead><tbody>` + result.data.files.map(f => `<tr><td><a href="/${path}/${f.name}">${f.name}</a></td><td>${f.modifiedTime}</td><td>${humanFileSize(f.size)}</td></tr>`).join(' ') + '</tbody></table>')
   } else {
   const res = await files.get({
       fileId: file.id,
