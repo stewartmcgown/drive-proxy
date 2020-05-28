daeabeace38f7f927666e7df9f077ee8637c3d39
Stewart <stewartcallummcgown@gmail.com>
2020-05-28 01:03:29 +0100

    fix(): for vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,28 +7,27 @@ interface Req {
   url: string;
 }
 
-dotenv.config();
-
-  const server = turbo.createServer(async function (req: Req, res) {
+export async function handler(req: Req, res) {
    
-    try {
-      const stream = await resolvePath(req.url.slice(1));
+  try {
+    const stream = await resolvePath(req.url.slice(1));
 
-      stream.on('data', d => {
-        res.write(Buffer.from(d));
-      })
+    stream.on('data', d => {
+      res.write(Buffer.from(d));
+    })
 
-      stream.on('end', () => res.end())
-    } catch (e) {
-      console.error(e)
-      let buffer =  Buffer.from('404');
-      res.statuscode = 404
-      res.setHeader('Content-Length', buffer.length)
-    res.write(buffer)
-    }
+    stream.on('end', () => res.end())
+  } catch (e) {
+    console.error(e)
+    let buffer =  Buffer.from('404');
+    res.statuscode = 404
+    res.setHeader('Content-Length', buffer.length)
+  res.write(buffer)
+  }
 
-    
-  })
+  
+}
 
-server.listen(3111)
+module.exports = handler;
+
 
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import * as turbo from 'turbo-http'
+import * as dotenv from 'dotenv'
+import handler from './api'
+
+dotenv.config();
+
+const server = turbo.createServer(handler)
+
+server.listen(3111)
